1983e80348c6d212f12f3dc5541059b472cc56b2
ngckinhluan <nkluantran2907@gmail.com>
2024-09-06 19:55:58 +0700

    Add new projects

---
+++ Service.Contracts/Interfaces/ICompanyService.cs
@@ -0,0 +1,15 @@
+﻿using Shared.DataTransferObjects;
+
+namespace Service.Contracts.Interfaces;
+
+public interface ICompanyService
+{
+    IEnumerable<CompanyDto> GetAllCompanies(bool trackChanges);
+    CompanyDto GetCompany(Guid companyId, bool trackChanges);
+    CompanyDto CreateCompany(CompanyForCreationDto company);
+    IEnumerable<CompanyDto> GetByIds(IEnumerable<Guid> ids, bool trackChanges);
+    (IEnumerable<CompanyDto> companies, string ids) CreateCompanyCollection
+        (IEnumerable<CompanyForCreationDto> companyCollection);
+    void DeleteCompany(Guid companyId, bool trackChanges);
+    void UpdateCompany(Guid companyId, CompanyForUpdateDto companyForUpdate, bool trackChanges);
+}
