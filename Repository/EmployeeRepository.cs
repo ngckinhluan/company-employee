4c6db2716a33a04b055f458ecf106c04e55cfb71
ngckinhluan <nkluantran2907@gmail.com>
2024-09-07 14:59:18 +0700

    Add async / await for Employee

--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Contracts.Interfaces;
 using Entities.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Repository;
 
@@ -10,14 +11,14 @@ internal sealed class EmployeeRepository : RepositoryBase<Employee>, IEmployeeRe
 	{
 	}
 
-	public IEnumerable<Employee> GetEmployees(Guid companyId, bool trackChanges) =>
+	public async Task<IEnumerable<Employee>> GetEmployeesAsync(Guid companyId, bool trackChanges) => await 
 		FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges)
 		.OrderBy(e => e.Name)
-		.ToList();
+		.ToListAsync();
 
-	public Employee GetEmployee(Guid companyId, Guid id, bool trackChanges) =>
+	public async Task<Employee> GetEmployeeAsync(Guid companyId, Guid id, bool trackChanges) => await 
 		FindByCondition(e => e.CompanyId.Equals(companyId) && e.Id.Equals(id), trackChanges)
-		.SingleOrDefault();
+		.SingleOrDefaultAsync();
 
 	public void CreateEmployeeForCompany(Guid companyId, Employee employee)
 	{
